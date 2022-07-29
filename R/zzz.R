f5e932e39d54f2b2a46e4328d3ec50892234c47e
Eve Perry <eve@eveperry.com>
2022-07-29 16:49:41 -0700

    setup thematic

--- R/zzz.R
+++ R/zzz.R
@@ -2,4 +2,11 @@
   bhhi_reactable_options()
 
   ggplot2::theme_set(bhhi_ggplot_theme())
+
+  thematic::thematic_rmd(
+    bg = "#fff3d9",
+    fg = "black",
+    accent = "#78c2ad",
+    font = "Lato"
+  )
 }

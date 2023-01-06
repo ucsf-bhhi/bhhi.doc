7ec806d363e3309dea2ee0b08e3a8bb971c399cb
Eve Perry <eve.perry@ucsf.edu>
2023-01-05 18:08:26 -0800

    default bhhi_palette() to unnamed vector

--- R/bhhi_palette.R
+++ R/bhhi_palette.R
@@ -5,6 +5,8 @@
 #' @param colors A character vector with the name(s) of the requested color(s).
 #'   The color names are: "orange", "green", "blue", "purple", "grey", "red",
 #'   "yellow", and "light blue". Leave empty to return the entire palette.
+#' @param names Should the returned vector have named values? Defaults to FALSE
+#'   to play nicely with ggplot.
 #'
 #' @return A character vector with the hex codes for the requested color
 #'   name(s).
@@ -12,9 +14,10 @@
 #'
 #' @examples
 #' bhhi_palette("red")
+#' bhhi_palette("red", names = TRUE)
 #' bhhi_palette(c("orange", "green"))
 #' bhhi_palette()
-bhhi_palette = function(colors) {
+bhhi_palette = function(colors, names = FALSE) {
   bhhi_palette = thematic::thematic_theme(
     bg = bhhi_bg(), fg = bhhi_bg(), accent = bhhi_accent()
   )$qualitative
@@ -23,7 +26,14 @@ bhhi_palette = function(colors) {
     "orange", "green", "blue", "purple", "grey", "red", "yellow", "light blue"
   )
 
-  bhhi_palette[colors]
+  return_colors = bhhi_palette[colors]
+
+  # ggplot doesn't like named hex code vectors so remove them by default
+  if (!names) {
+    names(return_colors) <- NULL
+  }
+
+  return_colors
 }
 
 bhhi_bg = function() "#fff3d9"
